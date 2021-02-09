a124128b77572bbeaee528e7b73e43a06f0cd461
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-02-09 10:03:46 +0100

    IntegerLista

--- Program.cs
+++ Program.cs
@@ -10,6 +10,41 @@ namespace Samlingsklasser
     {
         static void Main(string[] args)
         {
+            //Skapa en lista av int
+            List<int> lista = new List<int>();
+
+            //Fyll listan med några enstaka värden
+            lista.Add(5);
+            lista.Add(15);
+            lista.Add(25);
+
+            //Lägg till flera tal samtidigt genom att använda en Array med värden
+            lista.AddRange(new int[] { 1, 2, 3, 4, 5, 6 });
+
+            //"Iterera" genom listan och skriv ut varje enskilt värde och index.
+            //Indexeringen är precis som med Array men istället för .Length finns det en .Count
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine("Index: {0} Tal: {1}", i, lista[i]);
+            }
+
+            //Undersök om ett visst värde finns i listan
+            Console.WriteLine("Finns talet 5 i listan? " + lista.Contains(5));
+            Console.WriteLine("Finns talet 7 i listan? " + lista.Contains(7));
+
+            //Ta bort första förekomst av ett värde
+            lista.Remove(5);
+
+            //Ta bort värde på ett angivet index
+            lista.RemoveAt(3);
+
+            //"Iterera" igen, denna gång med en foreach loop
+            foreach (int tal in lista)
+            {
+                Console.WriteLine("Tal: {0}", tal);
+            }
+
+            Console.ReadKey();
         }
     }
 }
